0468a4e269b5fc07d9d49d93347304e00e96b18a
Roman <mister02.rv@gmail.com>
2022-02-20 12:42:35 +0200

    update eslint config

--- backoffice/.eslintrc.js
+++ backoffice/.eslintrc.js
@@ -3,13 +3,17 @@ module.exports = {
   parserOptions: {
     project: 'tsconfig.json',
     sourceType: 'module',
+    tsconfigRootDir: './'
   },
   plugins: ['@typescript-eslint/eslint-plugin'],
   extends: [
+    'airbnb-base',
     'plugin:@typescript-eslint/eslint-recommended',
     'plugin:@typescript-eslint/recommended',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:import/typescript',
     'prettier',
-    'prettier/@typescript-eslint',
   ],
   root: true,
   env: {
@@ -20,5 +24,14 @@ module.exports = {
     '@typescript-eslint/interface-name-prefix': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    'class-methods-use-this': 'off',
+    'import/prefer-default-export': 'off',
+    'import/order': 'off',
+    'import/no-extraneous-dependencies': 'off',
+    'no-useless-constructor': 'off',
+    'no-restricted-globals': 'off',
+    'import/no-unresolved': 'off',
+    'import/extensions': 'off',
+    'max-classes-per-file': 'warn',
   },
 };
