f0204c3b55b3652f0501baf46ca4094922a2d665
Roman <mister02.rv@gmail.com>
2022-03-06 09:06:34 +0200

    update main files

--- backoffice/src/app.module.ts
+++ backoffice/src/app.module.ts
@@ -1,10 +1,29 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from '@nestjs/common'
+import { ConfigModule } from '@nestjs/config'
+
+import { DatabaseModule } from './database.module'
+import { UsersModule } from './modules/users/users.module'
+import { configuration } from './config/configuration'
+import { AuthenticationModule } from './modules/authentication/authentication.module'
+import { EmailConfirmationModule } from './modules/email-confirmation/email-confirmation.module'
+import { EmailModule } from './modules/mailer/email.module'
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: `${process.cwd()}/src/config/env/${
+        process.env.NODE_ENV
+      }.env`,
+      isGlobal: true,
+      load: [configuration],
+    }),
+    DatabaseModule,
+    UsersModule,
+    AuthenticationModule,
+    EmailConfirmationModule,
+    EmailModule,
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
--- backoffice/src/main.ts
+++ backoffice/src/main.ts
@@ -1,8 +1,21 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from '@nestjs/core'
+import { NestExpressApplication } from '@nestjs/platform-express'
+import { ConfigService } from '@nestjs/config'
+
+import { AppModule } from './app.module'
+import { setupSwagger } from 'viveo-swagger'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule)
+
+  const configService: ConfigService = app.get(ConfigService)
+
+  if (configService.get<string>('NODE_ENV') === 'development') {
+    setupSwagger(app)
+  }
+
+  const port = configService.get<number>('port')
+
+  await app.listen(port)
 }
-bootstrap();
+bootstrap()
