d6feca38e4662e5b0517950e478ef2813a32e0ab
Roman <mister02.rv@gmail.com>
2022-04-02 10:26:44 +0300

    update providers

---
+++ backoffice/src/providers/stream-status-transition.service.ts
@@ -0,0 +1,25 @@
+import { StreamStatus, IStreamStatusMap } from '../common'
+
+export class StreamStatusTransitionService {
+  private static streamStatusMap: IStreamStatusMap = {
+    [StreamStatus.Created]: [StreamStatus.Activating],
+    [StreamStatus.Activating]: [StreamStatus.Activated],
+    [StreamStatus.Activated]: [StreamStatus.Started],
+    [StreamStatus.Started]: [StreamStatus.Stopped, StreamStatus.Deactivating],
+    [StreamStatus.Stopped]: [StreamStatus.Deactivating],
+    [StreamStatus.Deactivating]: [StreamStatus.Deactivated],
+  }
+
+  static validateTransition(
+    streamStatus: StreamStatus,
+    statusTransition: StreamStatus,
+  ): boolean {
+    const allowedTransitions = this.streamStatusMap[streamStatus]
+
+    // return allowedTransitions
+    //   ? allowedTransitions.includes(statusTransition)
+    //   : false
+
+    return !!allowedTransitions?.includes(statusTransition)
+  }
+}
