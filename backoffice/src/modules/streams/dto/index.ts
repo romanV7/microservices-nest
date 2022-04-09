e33a8fd7edacb9bab2e45d302c0e02e61ebd39a8
Roman <mister02.rv@gmail.com>
2022-04-09 08:59:10 +0300

    add stream dtos

---
+++ backoffice/src/modules/streams/dto/index.ts
@@ -0,0 +1,4 @@
+export * from './create-stream.dto'
+export * from './stream.dto'
+export * from './update-stream'
+export * from './complete-stream'
---
+++ backoffice/src/modules/streams/dto/update-stream.ts
@@ -0,0 +1,40 @@
+import { Type } from 'class-transformer'
+import { IsDate, IsEnum, IsNumber, IsString } from 'class-validator'
+import { StreamType } from '../../../common'
+import {
+  StreamArchieveUrl,
+  StreamInboundUrl,
+  StreamOutboundUrl,
+  StreamTitle,
+} from '../../../decorators'
+
+export class UpdateStreamDto {
+  @IsString()
+  @StreamTitle()
+  title: string
+
+  @IsDate()
+  scheduledFor: Date
+
+  @Type(() => Number)
+  @IsNumber()
+  scheduledDuration: number
+
+  @IsEnum(StreamType)
+  type: StreamType
+
+  @StreamInboundUrl()
+  inboundUrl: string
+
+  @IsString()
+  @StreamOutboundUrl()
+  outboundUrl: string
+
+  @IsString()
+  @StreamArchieveUrl()
+  archiveUrl: string
+
+  @Type(() => Number)
+  @IsNumber()
+  maxViewersCount: number
+}
