01472cc797bebefb60755d2ffdc088464bbdbdf5
Roman <mister02.rv@gmail.com>
2022-04-09 09:00:06 +0300

    add stream module

---
+++ backoffice/src/modules/streams/streams.module.ts
@@ -0,0 +1,41 @@
+import { Module } from '@nestjs/common'
+import { StreamsService } from './streams.service'
+import { StreamsController } from './streams.controller'
+import { TypeOrmModule } from '@nestjs/typeorm'
+import { StreamEntity } from './streams.entity'
+import { RedisCacheModule } from '../redis/redis-cache.module'
+import { UsersModule } from '../users/users.module'
+import {
+  BaseHttpConnectorService,
+  provideConstantValue,
+  DigitalOceanStreamProviderService,
+} from '../../providers'
+
+@Module({
+  imports: [
+    TypeOrmModule.forFeature([StreamEntity]),
+    RedisCacheModule,
+    UsersModule,
+  ],
+  controllers: [StreamsController],
+  providers: [
+    StreamsService,
+    DigitalOceanStreamProviderService,
+    BaseHttpConnectorService,
+    provideConstantValue<string>('STREAM_PROVIDER_URL', 'streamProvider.url'),
+    provideConstantValue<number>(
+      'STREAM_PROVIDER_REQUEST_TIMEOUT',
+      'streamProvider.requestTimeout',
+    ),
+    provideConstantValue<string>(
+      'STREAM_PROVIDER_USERNAME',
+      'streamProvider.username',
+    ),
+    provideConstantValue<string>(
+      'STREAM_PROVIDER_PASSWORD',
+      'streamProvider.password',
+    ),
+  ],
+  exports: [StreamsService],
+})
+export class StreamsModule {}
