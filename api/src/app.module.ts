8f6f859bac47ed3bd2579d1786989f1506d86ed2
Roman <mister02.rv@gmail.com>
2022-04-13 14:21:25 +0300

    add main

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,10 +1,17 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from '@nestjs/common'
+import { ConfigModule } from '@nestjs/config'
+import { DatabaseModule } from './database.module'
+import { configuration } from './config/configuration'
+import { StreamsModule } from './modules/streams/streams.module'
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [configuration],
+    }),
+    DatabaseModule,
+    StreamsModule,
+  ],
 })
 export class AppModule {}
--- api/src/main.ts
+++ api/src/main.ts
@@ -1,8 +1,50 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from '@nestjs/core'
+import { NestExpressApplication } from '@nestjs/platform-express'
+import { ConfigService } from '@nestjs/config'
+import { BadRequestException, ValidationPipe } from '@nestjs/common'
+import { AppModule } from './app.module'
+import { setupSwagger } from 'viveo-swagger'
+import { HttpExceptionFilter } from './filters'
+import { errorParser, ResponseErrorTypeEnum } from './common'
+import { useContainer } from 'typeorm'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
+    logger: ['debug', 'error', 'log', 'verbose', 'warn'],
+  })
+
+  const httpExceptionFilter = new HttpExceptionFilter()
+
+  const validationPipe = new ValidationPipe({
+    exceptionFactory: errors =>
+      new BadRequestException({
+        type: ResponseErrorTypeEnum.SCHEMA_VALIDATION_ERROR,
+        errors: errorParser(errors),
+      }),
+    forbidNonWhitelisted: true,
+    forbidUnknownValues: true,
+    whitelist: true,
+    transform: true,
+    transformOptions: {
+      enableImplicitConversion: true,
+    },
+  })
+
+  const configService: ConfigService = app.get(ConfigService)
+
+  if (configService.get<string>('NODE_ENV') === 'development') {
+    setupSwagger(app)
+  }
+
+  const port = configService.get<number>('port')
+
+  app.useGlobalPipes(validationPipe).useGlobalFilters(httpExceptionFilter)
+
+  useContainer(app.select(AppModule), {
+    fallback: true,
+    fallbackOnErrors: true,
+  })
+
+  await app.listen(port)
 }
-bootstrap();
+bootstrap()
